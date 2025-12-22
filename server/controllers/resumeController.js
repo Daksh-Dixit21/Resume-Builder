38aa957cd78eb17f144ddeed53d77c0dd6f310cd
Daksh-Dixit21 <dixitdaksh21@gmail.com>
2025-12-22 17:12:10 +0530

    Added docker file to solve pupeeter issue

--- server/controllers/resumeController.js
+++ server/controllers/resumeController.js
@@ -95,7 +95,12 @@ export const downloadPdf = async (req, res) => {
 
         const browser = await puppeteer.launch({
             headless: 'new',
-            args: ['--no-sandbox', '--disable-setuid-sandbox']
+            args: [
+                '--no-sandbox',
+                '--disable-setuid-sandbox',
+                '--disable-dev-shm-usage', // critical for docker
+                '--disable-gpu'
+            ]
         });
         const page = await browser.newPage();
 
